c94d7ab826cee5f4d9f3538f5e094ad6430ef731
Team E-Commerce <team-e-commerce@e-spirit.com>
2022-05-11 15:51:11 +0200

    "Updating to v1.2.0"

--- index.js
+++ index.js
@@ -1,37 +1,22 @@
 'use strict';
 require('dotenv').config();
+const BridgeCommons = require('fcecom-bridge-commons');
 const path = require('path');
-const http = require('http');
 
-const oas3Tools = require('oas3-tools');
-const serverPort = process.env.BRIDGE_SERVER_PORT;
+const { BRIDGE_AUTH_USERNAME, BRIDGE_AUTH_PASSWORD, CONN_MODE, SSL_KEY, SSL_CERT } = process.env;
 
-const { BRIDGE_AUTH_USERNAME, BRIDGE_AUTH_PASSWORD, auth = BRIDGE_AUTH_PASSWORD && `${BRIDGE_AUTH_USERNAME}:${BRIDGE_AUTH_PASSWORD}` } = process.env;
+const port = process.env.NODE_PORT || process.env.PORT || 3000;
 
-// basic auth
-function validate(request) {
-  return request.headers.authorization === `Basic ${Buffer.from(auth).toString('base64')}`;
-}
-
-// swaggerRouter configuration
-const options = {
-  routing: {
-    controllers: path.join(__dirname, './controllers'),
+BridgeCommons({
+  username: BRIDGE_AUTH_USERNAME,
+  password: BRIDGE_AUTH_PASSWORD,
+  servicesDir: path.join(process.cwd(), './src/service'),
+  port: port,
+  features: {
+    contentPages: true,
+    categoryTree: true
   },
-  openApiValidator: {
-    validateSecurity: {
-      handlers: {
-        basicAuth: validate,
-      },
-    },
-  },
-};
-
-const expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
-const app = expressAppConfig.getApp();
-
-// Initialize the Swagger middleware
-http.createServer(app).listen(serverPort, function () {
-  console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-  console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+  useSsl: CONN_MODE === 'HTTPS',
+  sslCert: SSL_CERT,
+  sslKey: SSL_KEY
 });
