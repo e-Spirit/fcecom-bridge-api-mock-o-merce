5e57d570d041867e2a93bfdc615af3a4bebe8c0d
Team E-Commerce <team-e-commerce@e-spirit.com>
2022-06-13 16:02:25 +0200

    "Updating to v1.3.1"

--- src/utils/http-client.js
+++ src/utils/http-client.js
@@ -1,19 +1,20 @@
 const axios = require('axios');
+const logger = require('../utils/logger')
 
 const { MOCKOMERCE_URL } = process.env;
 const client = axios.create({ baseURL: MOCKOMERCE_URL });
 
 client.interceptors.response.use(
   (response) => {
-    console.log(` ↳ ${response.config.method.toUpperCase()} ${response.config.url} - ${response.status} ${response.statusText}`);
+    logger.logInfo(` ↳ ${response.config.method.toUpperCase()} ${response.config.url} - ${response.status} ${response.statusText}`);
     return response;
   },
   (error) => {
     const { message, response } = (error);
     if (response) {
-      console.error(` ↳ ${response.config.method.toUpperCase()} ${response.config.url} - ${response.status} ${response.statusText}\n   ${message}`);
+      logger.logError(` ↳ ${response.config.method.toUpperCase()} ${response.config.url} - ${response.status} ${response.statusText}\n   ${message}`);
     } else {
-      console.error(` ↳ ${message}`);
+      logger.logError(` ↳ ${message}`);
     }
     return Promise.resolve({ error: true });
   }
